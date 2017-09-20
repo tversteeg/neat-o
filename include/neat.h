3a27febc1582bff04f1a5969af50ca0adec4ec69
Thomas Versteeg <thomasversteeg@gmx.com>
2017-09-20 22:57:06 +0200

    Small indentation fix

--- include/neat.h
+++ include/neat.h
@@ -7,8 +7,8 @@ typedef void* neat_ffnet_t;
 neat_population_t neat_population_create();
 
 neat_gen_t neat_run(neat_population_t population,
-		       void(*fitness_func)(neat_gen_t *gens),
-		       int generations);
+		    void(*fitness_func)(neat_gen_t *gens),
+		    int generations);
 
 neat_ffnet_t neat_ffnet_create(neat_gen_t winner);
 neat_ffnet_t neat_ffnet_activate(neat_ffnet_t net, double* inputs, int ninputs);
