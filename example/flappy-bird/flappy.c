1c985dc5978fdeb9455bdd5cc6815f029216b904
Thomas Versteeg <thomasversteeg@gmx.com>
2017-11-11 23:11:20 +0100

    Render flappy birds with ncurses and fix physics

--- example/flappy-bird/flappy.c
+++ example/flappy-bird/flappy.c
@@ -12,9 +12,11 @@
 /* Frames per second */
 #define FPS 24u
 
+#define WIDTH 80
+#define HEIGHT 24
+
 #define GRAVITY 0.05f
 #define UP_KEY_VELOCITY -0.5f
-#define HEIGHT 80
 
 #define POPULATION 100
 
@@ -31,14 +33,11 @@ static struct bird birds[POPULATION];
 static struct neat_config config;
 static neat_t neat;
 
-static int current_time;
-
 static int get_bird_height(struct bird b)
 {
-	float velocity_since_last, gravity_since_last;
-	int time_squared;
+	float velocity_since_last, gravity_since_last, time_squared;
 
-	velocity_since_last = UP_KEY_VELOCITY * b.last_time;
+	velocity_since_last = UP_KEY_VELOCITY * (float)b.last_time;
 
 	time_squared = b.last_time * b.last_time;
 
@@ -47,43 +46,128 @@ static int get_bird_height(struct bird b)
 	return b.last_height + (int)(velocity_since_last + gravity_since_last);
 }
 
+static struct bird get_farthest_bird(void)
+{
+	size_t i;
+	int farthest = 0;
+	struct bird farthest_bird;
+
+	for(i = 0; i < POPULATION; i++){
+		struct bird b;
+
+		b = birds[i];
+		if(b.x > farthest){
+			farthest = b.x;
+			farthest_bird = b;
+		}
+	}
+	
+	return farthest_bird;
+}
+
+static void draw_birds(void)
+{
+	size_t i;
+	int middle;
+	struct bird farthest;
+
+	farthest = get_farthest_bird();
+	middle = farthest.x - WIDTH / 2;
+
+	/* Draw the ground */
+	mvhline(HEIGHT - 2, 1, 0, WIDTH - 2);
+
+	/* Draw a moving figure to get a sense of speed */
+    	mvaddch(HEIGHT - 2, (WIDTH - 1) - farthest.x % (WIDTH - 2), ACS_BTEE);
+
+	for(i = 0; i < POPULATION; i++){
+		struct bird *b;
+		int real_x, real_y;
+
+		b = birds + i;
+		
+		real_x = b->x - middle;
+		real_y = HEIGHT - get_bird_height(*b) - 2;
+
+		/* Ignore the bird if it's not in the screen */
+		if(real_x < 1 || real_x >= WIDTH - 1){
+			continue;
+		}
+		if(real_y < 1 || real_y >= HEIGHT - 1){
+			continue;
+		}
+
+		mvprintw(real_y, real_x, "o");
+	}
+
+	mvprintw(HEIGHT + 1, 1, "%d", farthest.x);
+}
+
+static void reset_bird(struct bird *b)
+{
+	assert(b);
+
+	b->last_height = HEIGHT / 2;
+	b->last_time = 0;
+	b->x = 0;
+}
+
+static void run_network_on_bird(struct bird *b, size_t index)
+{
+	const float *results;
+	float height_norm, dist_to_middle, fitness;
+	int height;
+
+	assert(b);
+
+	height = get_bird_height(*b);
+
+	/* Run the bird network */
+	height_norm = height / (float)HEIGHT;
+	results = neat_run(neat, index, &height_norm);
+	assert(results);
+
+	/* Make the bird jump if the output neuron is triggered */
+	if(*results > 0.5f){
+		b->last_height = height;
+		b->last_time = 0;
+	}
+
+	/* We want the bird to stay as close to the middle as possible but also
+	 * to get as far as possible
+	 */
+	dist_to_middle = (height - HEIGHT / 2) / (float)HEIGHT;
+	fitness = dist_to_middle * 0.1f + (b->x / 100.0f);
+	neat_set_fitness(neat, index, fitness);
+}
+
 static void population_tick(void)
 {
 	size_t i;
 
 	for(i = 0; i < POPULATION; i++){
-		const float *results;
 		int height;
 		struct bird *b;
-		float fitness, dist_to_middle;
 
 		b = birds + i;
 
+		b->x++;
+		b->last_time++;
 		height = get_bird_height(*b);
 
-		/* Run the bird network */
-		results = neat_run(neat, i, &height);
-		assert(results);
-
-		/* Make the bird jump if the output neuron is triggered */
-		if(*results > 0.5f){
-			b->last_height = height;
-			b->last_time = current_time;
+		/* Reset the bird if it's out of bounds */
+		if(height > HEIGHT || height < 0){
+			reset_bird(b);
 		}
 
-		/* We want the bird to stay as close to the middle as possible
-		 * but also to get as far as possible
-		 */
-		dist_to_middle = (height - HEIGHT / 2) / (float)HEIGHT;
-		fitness = dist_to_middle * 0.1f + (b->x / 100.0f)
-		neat_set_fitness(neat, i, fitness);
+		if(b->last_time > 10){
+			run_network_on_bird(b, i);
+		}
 
 		neat_increase_time_alive(neat, i);
 	}
 
 	neat_epoch(neat, NULL);
-
-	current_time++;
 }
 
 static void initialize_population(void)
@@ -96,16 +180,14 @@ static void initialize_population(void)
 	config.network_hidden_nodes = 6;
 	config.population_size = POPULATION;
 
+	config.genome_minimum_ticks_alive = 50;
+	config.minimum_time_before_replacement = 200;
+
 	neat = neat_create(config);
 	assert(neat);
 
 	for(i = 0; i < POPULATION; i++){
-		struct bird *b;
-
-		b = birds + i;
-		b->last_time = 0;
-		b->last_height = 0;
-		b->x = 0;
+		reset_bird(birds + i);
 	}
 }
 
@@ -117,6 +199,18 @@ static void initialize_ncurses(void)
 	noecho();
 	curs_set(0);
 	timeout(0);
+}
+
+static void draw_ncurses_rectangle(int y1, int x1, int y2, int x2)
+{
+    mvhline(y1, x1, 0, x2-x1);
+    mvhline(y2, x1, 0, x2-x1);
+    mvvline(y1, x1, 0, y2-y1);
+    mvvline(y1, x2, 0, y2-y1);
+    mvaddch(y1, x1, ACS_ULCORNER);
+    mvaddch(y2, x1, ACS_LLCORNER);
+    mvaddch(y1, x2, ACS_URCORNER);
+    mvaddch(y2, x2, ACS_LRCORNER);
 }
 
 int main(int argc, char *argv[])
@@ -132,8 +226,6 @@ int main(int argc, char *argv[])
 
 	initialize_population();
 
-	current_time = 0;
-
 	run = true;
 	while(run){
 		/* Get the key pressed */
@@ -146,6 +238,11 @@ int main(int argc, char *argv[])
 		clear();
 
 		population_tick();
+
+		draw_ncurses_rectangle(0, 0, HEIGHT, WIDTH);
+		draw_birds();
+
+		refresh();
 
 		usleep(1000000u / FPS);
 	}
